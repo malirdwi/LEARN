5a163d63739c79e3c740fec02edddb6ad59d3cea
malirdwi <50963361+malirdwi@users.noreply.github.com>
2020-01-13 19:01:37 -0500

    Update checkToolbox.m

--- Subroutines/checkToolbox.m
+++ Subroutines/checkToolbox.m
@@ -3,9 +3,9 @@ function [optim,symmath]=checkToolbox
 v = ver;
 optim = any(strcmp(cellstr(char(v.Name)), 'Optimization Toolbox'))
 if optim==0
-fprintf(1,'Optimization Toolbox is missing. Please install it first')
+error('Optimization Toolbox is missing. Please install it first')
 end
 symmath = any(strcmp(cellstr(char(v.Name)), 'Symbolic Math Toolbox'))
 if symmath==0
-fprintf(1,'Symbolic Math Toolbox is missing. Please install it first')
+error('Symbolic Math Toolbox is missing. Please install it first')
 end
