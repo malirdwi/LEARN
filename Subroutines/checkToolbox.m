4867a5078efd31e8464df66f32c03a6d782ce7af
malirdwi <50963361+malirdwi@users.noreply.github.com>
2020-01-14 18:49:55 -0500

    Update checkToolbox.m

--- Subroutines/checkToolbox.m
+++ Subroutines/checkToolbox.m
@@ -4,8 +4,17 @@ v = ver;
 optim = any(strcmp(cellstr(char(v.Name)), 'Optimization Toolbox'));
 if optim==0
 error('Optimization Toolbox is missing. Please install it before running this subroutine');
-end
+elseif ~exist('linprog','file')
+        error('Optimization Toolbox is missing. Please install it before running this subroutine');
+    end
 symmath = any(strcmp(cellstr(char(v.Name)), 'Symbolic Math Toolbox'));
 if symmath==0
 error('Symbolic Math Toolbox is missing. Please install it before running this subroutine');
-end
+elseif ~exist('syms','file')
+        error('Symbolic Math Toolbox is missing. Please install it before running this subroutine');
+    end
+       
+    
+if ~exist('cvx_begin','file')
+        error('cvx is missing. Please install it via cvxr.com/cvx/download before running this subroutine');
+    end
