2cb2f11dfb50e60f4a5d2411285c779a0d0e1904
malirdwi <50963361+malirdwi@users.noreply.github.com>
2020-01-13 19:36:37 -0500

    Update RobustNondegeneracy.m

--- Subroutines/RobustNondegeneracy.m
+++ Subroutines/RobustNondegeneracy.m
@@ -1,28 +1,30 @@
-
 function flag=RobustNondegeneracy(G)
 checkToolbox
-
 
 [n,r]=size(G);
 oldparam = sympref('HeavisideAtOrigin',0);
 
-rho=heaviside(-G)'.*ones(r,n);%sym('r',[ r n]);
-rho=G*rho;
+rho0=heaviside(-G)'.*ones(r,n);%sym('r',[ r n]);
+rho=G*rho0;
 
 V=null(G','r')';
 d=size(V,1);
 
-I=eye(n);
+f=0;
+while(f==0)
+I=rand(n);
 
-T=[I(:,1:n-d)'; V];
+T=[I(:,1:n-d)'; V]
+if(rank(T)==n)
+    f=1;
+end
+end
 
 J=T*rho*inv(T);
 
 Jr=J(1:n-d,1:n-d);
 
 flag=n-d-rank(Jr);
-
-
 
 
 
