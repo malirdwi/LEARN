452df20c704051f57e10b11c88fd188ea346db68
malirdwi <50963361+malirdwi@users.noreply.github.com>
2020-01-15 12:58:40 -0500

    Update LEARNmain.m

--- LEARNmain.m
+++ LEARNmain.m
@@ -27,7 +27,7 @@ else
 end
     
 fprintf(file,'--------------------------------\n');
-fprintf(file,'Welcome to LEARN v1.0, May 2019\n');
+fprintf(file,'Welcome to LEARN v1.0, Jan 2020\n');
 fprintf(file,'Developed by M. Ali Al-Radhawi malirdwi@{northeastern.edu,mit.edu,gmail.com}\n\n');
 fprintf(file,'LEARN tries to construct a Robust Lyapunov Function for a given reaction network.\n');
 fprintf(file,'--------------------------------\n');
