8328a6a24ccc835eb33dad81ff1741dc3d378d2b
malirdwi <50963361+malirdwi@users.noreply.github.com>
2020-01-14 19:40:30 -0500

    Update ConstructLPAuto.m

--- Subroutines/ConstructLPAuto.m
+++ Subroutines/ConstructLPAuto.m
@@ -1,5 +1,4 @@
-function [Q,cvx,H2,Xi]=ConstructLP(A,B,varargin)
-checkToolbox
+function [Q,cvx,H2,Xi]=ConstructLPAuto(A,B,varargin)
  if nargin>2
 H2=varargin{2};
 else
@@ -15,32 +14,41 @@ cvx=0;
 if nargin>4
     cvx=varargin{4};
 end
+
+
   
     
  
+    G=B-A;
     
+    v=IsAS1(G);
+if(min(v)>0)
+    AS1=1;
+else 
+    AS1=0;
+    Q=[];
+    return;
+end
+
  
   H=[G;H2  ];
 
 
      
 
-
-V=null(G,'r');
+ V=null(G,'r');
  
 
- 
 
 [n r]=size(G);
 [ne r]=size(H);
 S=[0:2^ne-1]';
 S= 2*(double(dec2bin(S))-48-0.5);
- options = optimoptions('linprog','Display','off');
 
 S1=[];
 j=1;jj=1;
 for i=1:2^ne   %%% REMOVING INCONSISTENT SYSTEM OF INEQUALITES
-   
+    options = optimoptions('linprog','Display','off');
  [x,y,flag]=linprog(zeros(1,r),-diag(S(i,:))*H,-1e-5*ones(ne,1),[],[],zeros(r,1),[],[],options);
  if(flag>0)
      S1(j,:)=S(i,:);
@@ -53,22 +61,9 @@ end
 %S1=S; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"!!!!!!!!!!!!!!!!%%%%%%%%%%
 [m ne]=size(S1);
  
-
- 
-if(mod(m,2)>0)
-         Q=[];
-    return;
-else
-for j=1:m/2
-    if(max(abs(S1(j,:)+S1(m+1-j,:)))>0)
-           Q=[];
-    return;
-    end
-end
-end
  
 
- 
+%%%%%%%%% REFINING THE PARTITIONS
 
 
 
@@ -111,19 +106,10 @@ q=sym('q',[s 1]);
 [Z,Z1,Zx,Z1v]=RegNbhd(S1,H);
 
 
-[x y]=find(C==0);
-E=[];
-for i=1:length(x)
-     
-    E=[E  diag(S1(x(i),:))*H(:,y(i))];
-    
-end
-
-null(E','r')'
-
+ 
 
 Z2=Z1;
-  cvx_begin
+  cvx_begin quiet
 variable Q(m,r)
 variable L(m,ne)
 variable L0(m,r)
@@ -132,43 +118,49 @@ variable L2(m,ne,m)
     minimize(sum(sum(Q(1,1))))
  
  
-subject to 
-uy=[ 1:m/2     ]; 
+subject to
+ 
+uy=[ 1:m/2     ];% 4 5 6  8
 uy=[uy m+1-flip(uy)];
-Q(uy,r0).*C(uy,r0)>=0;  
+Q(uy,r0).*C(uy,r0)>=0;  %%% to have descreasing function
 % 
 for j=uy
     for l=1:r
         if(C(j,l)==0)
-            Q(j,l)==0;   
+            Q(j,l)==0;   %% zeros what have to be zeroed
         end
     end
 end 
  
- Q*V==0;    
+
+ Q*V==0;   % Kernel of the function must be subset of the ketnel of G
  L>=0;
  
-L0==0;  
-
+L0==0; %%%%%%%%%%%!!!!!!!!!!!!!!!!
+ 
 for j=1:m
     
  
-       sum(L(j,:))+sum(L0(j,:))>1;
-         sum(L(j,:))+sum(L0(j,:))<2.13848;
+       sum(L(j,:))+sum(L0(j,:))>=1; 
  
-   Q(j,:)== L(j,:)*diag(S1(j,:))*H+L0(j,:); 
+if w==1
+    Q(j,:)==L(1,:)*diag(S1(j,:))*H; 
+else
+   Q(j,:)== L(j,:)*diag(S1(j,:))*H; 
+end
  
-   
      if(j<=m/2)
          Q(j,:)==-Q(m-j+1,:);  
      end
- 
+     %%%%%% continu9ety  covexity
      ex=[       ];
-   ex=[ex flip(m+1-ex)];
+   ex=[ex wrev(m+1-ex)];
      Z2(:,ex)=0*Z2(:,ex);
      Z2(ex,:)=0*Z2(ex,:);
      
-  
+ 
+   
+   
    
     for j2=j+1:m
         
@@ -183,11 +175,14 @@ for j=1:m
  
             
     end
-    end
+end
+    
+if cvx==1
+    L2>=0
+end
+
 
 cvx_end 
-
- 
  
 
 
