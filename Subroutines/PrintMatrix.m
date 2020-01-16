fb9cc3aa285cf53d5731a92480ba7eded0a1e36a
malirdwi <50963361+malirdwi@users.noreply.github.com>
2020-01-16 12:57:10 -0500

    Update PrintMatrix.m

--- Subroutines/PrintMatrix.m
+++ Subroutines/PrintMatrix.m
@@ -1,5 +1,5 @@
 function PrintMatrix(C,file)
-%fprintf(file,' [ \n');
+fprintf(file,' [ \n');
        for ii = 1:size(C,1)
            for jj=1:size(C,2)
                if(C(ii,jj)>=0)
@@ -11,4 +11,4 @@ function PrintMatrix(C,file)
            fprintf(file,'\n');
 end
     
-%fprintf(file,' ] \n');
+fprintf(file,' ] \n');
